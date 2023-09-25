093cdd8fb35cb167841bf81b91aa4e11741d9f9c
Tim Miller <timothy.miller@childrens.harvard.edu>
2023-09-25 09:18:58 -0400

    Updates to mimic ctakes processing scripts to use new ctakes endpoint options and add mongo output.

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
     * Obtain access to the MIMIC III database (ask your PI).
     * Obtain a UMLS account and API key
     * Install Docker and docker-compose
+    * If writing to a MongoDB database:
+      * Create local directory: ```mkdir mimic_db```
+      * docker run --rm --name mongodb -d -v mimic_db:/data/db -p 27017:27017 mongo
 
 1. Setup cTAKES containers: 
     * ```git clone git@github.com:Machine-Learning-for-Medical-Language/ctakes-rest-package.git```
@@ -13,7 +16,17 @@
 
 2. Run the python script to process the data -- run with -h to receive detailed documentation of the options:
     * ```python process_mimic.py --input-path <path to NOTEEVENTS.csv file> --output-format <json|mongo|xmi|fhir> --output_dir <directory to write files if output format is file-based>```
+    * If you run with the flag ```--max-notes N```, you can run on a subset to make sure everything is working correctly before processing the whole dataset.
 
-3. For MongoDB usage, setup a few indices for faster querying:
-  a. TODO
+3. If you used MongoDB, check into some of the output:
+
+    ```$ mongo```
+
+    ```> use mimic```
+
+    ```> db.note_nlp.stats()['count']``` # Should return number of notes processed and entered into database
+
+    
+
+
 
--- process_mimic.py
+++ process_mimic.py
@@ -14,17 +14,25 @@ import pandas as pd
 from threading import Thread
 from queue import Queue
 
+import mongo_handler
+
 JSON = 'json'
 XMI = 'xmi'
 JSON_LITE = 'json-lite'
 FHIR = 'fhir'
 MONGO = 'mongo'
+FILTERED = 'filtered'
+MIMIC = 'mimic'
+I2B2 = 'i2b2'
 
 STOP_JOB = '-STOP-'
 NUM_TRIES = 5
 
 file_extensions = {JSON:JSON, XMI:XMI, JSON_LITE:JSON, FHIR:FHIR}
 output_formats = [JSON, XMI, JSON_LITE, FHIR, MONGO]
+input_formats = [MIMIC, I2B2]
+text_fields = { MIMIC: 'TEXT', I2B2: 'OBSERVATION_BLOB'}
+id_fields = { MIMIC: 'ROW_ID', I2B2: 'INSTANCE_NUM'}
 
 # One worker type just reads notes and metadata from a queue, calls a REST server, and
 # puts the output into the output queue
@@ -46,18 +54,20 @@ class InputWorker(Thread):
             tries = 0
             while tries < NUM_TRIES:
                 tries += 1
-                r = requests.post(self.rest_url, data=text, params=params)
-                if r is not None and r.status_code == 200:
-                    json = r.json()
-                    output_json = {'nlp':json, 'metadata': metadata}
-                    self.out_queue.put(output_json)
-                    self.in_queue.task_done()
-                    break
-                else:
-                    if tries == NUM_TRIES:
-                        sys.stderr.write('Could not process row with metadata %s\n' % (str(metadata)))
+                try:
+                    r = requests.post(self.rest_url, data=text, params=params)
+                    if r is not None and r.status_code == 200:
+                        json = r.json()
+                        output_json = {'nlp':json, 'metadata': metadata}
+                        self.out_queue.put(output_json)
+                        self.in_queue.task_done()
+                        break
+                    else:
+                        if tries == NUM_TRIES:
+                            sys.stderr.write('Could not process row with metadata %s\n' % (str(metadata)))
+                except:
+                    sys.stderr.write('Error on post call with metadata %s\n' % (str(metadata)))
 
-            
 
 # The other worker type reads the output from the NLP and stores it, either in a filesystem
 # or a database.
@@ -66,6 +76,8 @@ class OutputWorker(Thread):
         super().__init__()
         self.queue = output_queue
         self.args = args
+        if self.args.output_format == MONGO:
+            self.mongo_db = mongo_handler.get_db(self.args.input_format)
 
     def run(self):
         while True:
@@ -76,25 +88,29 @@ class OutputWorker(Thread):
             else:
                 json = job
 
-                if self.args.output_format == 'json':
+                if self.args.output_format == JSON or self.args.output_format == MONGO:
                     json['nlp'] = json['nlp']['_views']['_InitialView']
 
                 output = json
 
-                if self.args.output_format == 'fhir':
+                if self.args.output_format == FHIR:
                     # TODO call to Bin's library once it's pip installable
                     raise NotImplementedError('FHIR file output not implemented yet.')
 
-                if self.args.output_format in ['json', 'xmi', 'json-lite', 'fhir']:
+                if self.args.output_format in [JSON, XMI, JSON_LITE, FHIR]:
                     of_name = join(self.args.output_dir, '%s.%s' % (output['metadata']['ROW_ID'], file_extensions[self.args.output_format]))
                     with open(of_name, 'wt') as of:
                         of.write(dumps(output))
+                elif self.args.output_format == MONGO:
+                    self.mongo_db.note_nlp.insert_one(json)
+
                 self.queue.task_done()
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('input_file', help='Path to NOTEEVENTS.csv[.gz] file')
-    parser.add_argument('--output-format', choices=output_formats, default='json')
+    parser.add_argument('--input-format', choices=input_formats, default=MIMIC)
+    parser.add_argument('--output-format', choices=output_formats, default=JSON)
     parser.add_argument('--rest-url', help='Path to cTAKES REST URL')
     parser.add_argument('--max-notes', type=int, help='Max number of notes to process (for testing)', default=-1)
     parser.add_argument('--output-dir', help='Output dir (for file-based output formats', default=None)
@@ -108,20 +124,24 @@ def main():
     compression = None
     # Trying to transparently handle the gzipped or non-gzipped case
     if args.input_file.endswith('.csv.gz'):
-        #f = gzip.open(args.input_file, 'rt')
         compression = 'gzip'
     elif args.input_file.endswith('.csv'):
-        #f = open(args.input_file, 'rt')
         compression = None
     else:
         raise Exception('Input file must end with .csv[.gz]')
 
+    text_field = text_fields[args.input_format]
+    id_field = id_fields[args.input_format]
+ 
     params = {}
-    if args.output_format == 'json':
+    if args.output_format == JSON:
         # this filters out syntax but keeps all the semantic types
-        params['format'] = 'filtered'
-    elif args.output_format == 'xmi':
-        params['format'] = 'xmi'
+        params['format'] = FILTERED
+    elif args.output_format == XMI:
+        params['format'] = XMI
+    elif args.output_format == MONGO:
+        params['format'] = FILTERED
+        assert mongo_handler.check_mongo_connection(), "Could not connect to MongoDB"
 
     assert args.output_format == MONGO or not args.output_dir is None, 'If output format is a file-based method then output-dir must be defined.'
 
@@ -139,30 +159,40 @@ def main():
     writer = OutputWorker(output_queue, args)
     writer.start()
 
-    #csvreader = csv.DictReader(f)
-    with pd.read_csv(args.input_file, chunksize=1000, compression=compression) as reader:
-        for chunk in tqdm(reader):
-            for row in chunk.iterrows():
-                row = row[1]
-                #for row_ind, row in enumerate(tqdm(csvreader)):
-                # check whether the user specified for an early exit (usually for testing purposes)
-                if args.max_notes > 0 and row_ind >= args.max_notes:
-                    print('Exiting after %d notes due to input argument' % (args.max_notes))
-                    break
+    reader = pd.read_csv(args.input_file, chunksize=1000, compression=compression)
+    num_rows = 0
+    row_id = -1
 
-                while job_queue.full():
-                    # no need to put all this data into memory if we already have 100 notes queued up.
-                    sleep(1)
+    for chunk in tqdm(reader):
+        for row in chunk.iterrows():
+            row = row[1]
+            #for row_ind, row in enumerate(tqdm(csvreader)):
+            # check whether the user specified for an early exit (usually for testing purposes)
+            if args.max_notes > 0 and num_rows >= args.max_notes:
+                print('Exiting after %d notes due to input argument' % (args.max_notes))
+                break
 
-                text = row.pop('TEXT')
-                if args.resume:
-                    row_id = row['ROW_ID']
-                    if args.output_format in ['json', 'xmi', 'json-lite', 'fhir']:
-                        of_name = join(args.output_dir, '%s.%s' % (row_id, file_extensions[args.output_format]))
-                        if exists(of_name):
-                            continue
-                
-                job_queue.put( (text, params, dict(row)) )
+            while job_queue.full():
+                # no need to put all this data into memory if we already have 100 notes queued up.
+                sleep(1)
+
+            text = row.pop(text_field)
+            if type(text) != str or len(text) == 0:
+                print("Error processing row %d, after instance %d, either text wasn't a string or was length 0" % (num_rows, row_id))
+                continue
+
+            if args.resume:
+                row_id = row[id_field]
+                if args.output_format in [JSON, XMI, JSON_LITE, FHIR]:
+                    of_name = join(args.output_dir, '%s.%s' % (row_id, file_extensions[args.output_format]))
+                    if exists(of_name):
+                        continue
+            
+            job_queue.put( (text, params, dict(row)) )
+            num_rows += 1
+
+        if args.max_notes > 0 and num_rows >= args.max_notes:
+            break
 
     # after we've pushed all the jobs to the workers add the stop job so they know when to exit.
     for worker_ind, worker in enumerate(workers):
